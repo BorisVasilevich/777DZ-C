e9e6b2327977c1e25025575db6a45c4a416702ce
BorisVasilevich <121186717+BorisVasilevich@users.noreply.github.com>
2023-02-16 21:14:50 +0300

    work68

--- home68/Program.cs
+++ home68/Program.cs
@@ -4,7 +4,27 @@
 //m = 3, n = 2 -> A(m,n) = 29
 
 
+int FAckermans(int m, int n)
+{
+    int res = 0;
+if (m == 0)
+res = n + 1;
+if (m > 0 && n ==0)
+res = FAckermans(m - 1, 1);
+if (m > 0 && n > 0)
+{
+   res = FAckermans(m - 1, FAckermans(m, n -1)); 
+
+}
+return res;
+}
 
 
 
+Console.WriteLine("Введите число m: ");
+int m = Convert.ToInt32(Console.ReadLine());
 
+Console.WriteLine("Введите число n: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine(FAckermans(m, n));
